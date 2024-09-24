46f55808820c692cbf0decd66f125d7bd19a94ef
wylu1037 <wylu1037@gmail.com>
2024-09-24 08:56:55 +0800

    chore: nothing

---
+++ src/crypto/index.ts
@@ -0,0 +1,22 @@
+let isDone: boolean = false;
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+let tuple: [string, number] = ["", 1];
+
+interface Person {
+  name: string;
+  age: number;
+  address: string;
+  greet(): void;
+}
+
+let person: Person = {
+  name: "John",
+  age: 5,
+  address: "American",
+  greet() {
+    console.log("Hello World!");
+  },
+};
+
+person.greet();
