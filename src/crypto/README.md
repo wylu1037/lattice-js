dfb1dcde61aeca9a019cdfc7db5c98e8e10f6f78
wylu1037 <wylu1037@gmail.com>
2024-09-25 14:32:20 +0800

    chore: submit temporary change

---
+++ src/crypto/README.md
@@ -0,0 +1,10 @@
+## Concepts
+
+### Keccak、Ripemd160
+加密哈希函数
+
+### Pbkdf2、Scrypt
+key derivation function
+
+### Ripemd160
+
--- tests/common/base58.test.ts
+++ tests/common/base58.test.ts
@@ -1,16 +1,15 @@
-import { Base58Impl, Base58Interface } from "../../src/common/base58";
-import { assert } from "chai";
 import "mocha";
-import { it } from "mocha";
+import { Base58Impl, Base58Interface } from "../../src/common/base58";
+import { expect } from "chai";
 
 describe("Base58", function () {
-  describe("Checksum", function () {
+  describe("checksum", function () {
     it("should return 42a873ac when the input text is Hello World", function () {
       const base58: Base58Interface = new Base58Impl();
       const out = base58.checksum(Buffer.from("Hello World", "utf-8"));
       const actual = out.toString("hex");
       const expected = "42a873ac";
-      assert.equal(actual, expected);
+      expect(actual).to.equal(expected);
     });
   });
 });
