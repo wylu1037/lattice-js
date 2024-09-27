801eeafdef5eef41c9b8e874e659ad5510f4bfb9
wylu1037 <wylu1037@gmail.com>
2024-09-27 17:46:52 +0800

    chore: http

--- src/providers/http_provider.ts
+++ src/providers/http_provider.ts
@@ -1,5 +1,8 @@
 import axios, {AxiosRequestHeaders, AxiosResponse} from "axios";
 
+const JSON_RPC_ID: number = 1;
+const JSON_RPC_VERSION: string = "2.0";
+
 interface JsonRpcError {
     code?: number;
     message?: string;
@@ -20,10 +23,18 @@ interface JsonRpcBody {
 }
 
 class HttpProvider {
+    // base url that used to access node
+    baseUrl: string;
 
-    async post<T>(url: string, jsonRpcBody: JsonRpcBody, headers?: AxiosRequestHeaders): Promise<JsonRpcResponse<T>> {
+    // construct a http provider with base url
+    constructor(baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
+
+    // send a post request to node and return json-rpc response
+    async post<T>(jsonRpcBody: JsonRpcBody, headers?: AxiosRequestHeaders): Promise<JsonRpcResponse<T>> {
         try {
-            const response = await this.rawPost(url, jsonRpcBody, headers);
+            const response = await this.rawPost(jsonRpcBody, headers);
             return response.data as JsonRpcResponse<T>;
         } catch (error) {
             if (error instanceof Error) {
@@ -34,9 +45,9 @@ class HttpProvider {
         }
     }
 
-    private async rawPost(url: string, jsonRpcBody: JsonRpcBody, headers?: AxiosRequestHeaders): Promise<AxiosResponse> {
+    private async rawPost(jsonRpcBody: JsonRpcBody, headers?: AxiosRequestHeaders): Promise<AxiosResponse> {
         try {
-            return await axios.post(url, jsonRpcBody, {headers: headers || {}});
+            return await axios.post(this.baseUrl, jsonRpcBody, {headers: headers || {}});
         } catch (error) {
             if (error instanceof Error) {
                 throw error;
@@ -47,10 +58,52 @@ class HttpProvider {
     }
 }
 
+interface HttpClient {
+
+    httpProvider: HttpProvider;
+
+    getLatestBlock(accountAddress: string): Promise<LatestBlock>;
+}
+
+class HttpClientImpl implements HttpClient {
+
+    httpProvider: HttpProvider;
+
+    constructor(private provider: HttpProvider) {
+        this.httpProvider = provider;
+    }
+
+    // handel json-rpc response. when the response is null, throw error
+    // when the response internal error is not null, throw error
+    private handleJsonRpcResponse<T>(response: JsonRpcResponse<T>): T {
+        if (!response) {
+            throw new Error("response is null");
+        }
+        if (response.error) {
+            throw new Error(`${response.error.code}: ${response.error.message}`);
+        }
+        return response.result as T;
+    }
+
+    // according to the account address to query latest block info from chain.
+    // Params accountAddress: string, example: zltc_cXV46yWanovM6ZppX91ZbUEtN6vAU7GiF
+    // Returns LatestBlock
+    async getLatestBlock(accountAddress: string): Promise<LatestBlock> {
+        const response: JsonRpcResponse<LatestBlock> = await this.httpProvider.post({
+            id: JSON_RPC_ID,
+            jsonrpc: JSON_RPC_VERSION,
+            method: "latc_getCurrentTBDB",
+            params: [accountAddress],
+        })
+        return this.handleJsonRpcResponse<LatestBlock>(response);
+    }
+}
+
 interface LatestBlock {
+    // latest transaction height associated with account
     currentTBlockNumber: number;
     currentTBlockHash: string;
     currentDBlockHash: string;
 }
 
-export {JsonRpcBody, JsonRpcResponse, JsonRpcError, HttpProvider, LatestBlock};
+export {JsonRpcBody, JsonRpcResponse, JsonRpcError, HttpProvider, LatestBlock, HttpClient, HttpClientImpl};
--- tests/providers/http_providers.test.ts
+++ tests/providers/http_providers.test.ts
@@ -1,20 +1,14 @@
-import {HttpProvider, JsonRpcResponse, LatestBlock} from "../../src/providers/http_provider";
+import {HttpClient, HttpClientImpl, HttpProvider} from "../../src/providers/http_provider";
 import {expect} from "chai";
 
 describe('HttpProviders', () => {
     describe('Http Client', () => {
         it('should be able to send http request', async () => {
-            const client = new HttpProvider();
+            const provider = new HttpProvider("http://192.168.1.185:13000");
+            const client: HttpClient = new HttpClientImpl(provider);
             try {
-                const response: JsonRpcResponse<LatestBlock> = await client.post("http://192.168.1.185:13000", {
-                    id: 1,
-                    jsonrpc: '2.0',
-                    method: "latc_getCurrentTBDB",
-                    params: ["zltc_cXV46yWanovM6ZppX91ZbUEtN6vAU7GiF"]
-                });
+                const response = await client.getLatestBlock("zltc_cXV46yWanovM6ZppX91ZbUEtN6vAU7GiF");
                 expect(response).not.to.be.null;
-                expect(response.error).to.be.undefined;
-                expect(response.result).not.to.be.null;
             } catch (error) {
                 expect(error).to.be.null;
             }
