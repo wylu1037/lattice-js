d46b558bbecb503eabdd8737fb5282bd6a6b5770
wylu1037 <wylu1037@gmail.com>
2024-10-14 07:48:47 +0800

    chore: add settings.json

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -1,15 +1,15 @@
 export interface KeyPair {
-    // 32 bytes
-    privateKey: Buffer;
-    // 65 bytes, uncompressed public key
-    publicKey: Buffer;
+  // 32 bytes
+  privateKey: Buffer;
+  // 65 bytes, uncompressed public key
+  publicKey: Buffer;
 }
 
 export interface CryptoInterface {
-    // generate key pair
-    // return private key and public key
-    generateKeyPair(): KeyPair;
+  // generate key pair
+  // return private key and public key
+  generateKeyPair(): KeyPair;
 
-    // compress public key
-    compressPublicKey(publicKey: Buffer | string): Buffer;
+  // compress public key
+  compressPublicKey(publicKey: Buffer | string): Buffer;
 }
